3f39acac424efeef023e1699a4f6e76531521ce8
s29zhu <shashazhu1989@gmail.com>
2013-02-23 23:24:06 -0500

    encryption the file

--- abe-grs/owner/encryption.c
+++ abe-grs/owner/encryption.c
@@ -5,6 +5,9 @@
 #include <math.h>
 #include <gmp.h>
 #include "grs.h"
+#include "string.h"
+#include "stdbool.h"
+#include "message_handle.h"
 
 typedef struct {
   field_t Fq, Fq2, Eq;
@@ -57,8 +60,13 @@ void initialize(element_t *qR,
                 element_t *ele_att_hash, 
                 pairing_t *pairing,
                 element_t *g,
+                element_t *h,
+                element_t *C,
+                element_t *C_tilde,
                 element_t *Cy,
-                element_t *Cy_prime){
+                element_t *Cy_prime,
+                mpz_t *message_mpz,
+                element_t *message_ele){
    //initialize the attribute string 
     int i = 0;
     char *attr[NUM_SHARE];
@@ -73,6 +81,11 @@ void initialize(element_t *qR,
     attr[7] = "check string 1";
     attr[8] = "check string 2";
     element_init_G1(*g, *pairing);
+    element_init_G1(*h, *pairing);
+    element_init_G1(*C, *pairing);
+    element_init_GT(*C_tilde, *pairing);
+    mpz_init(*message_mpz);
+    element_init_GT(*message_ele, *pairing);
     //initialize the attributes' hash value
     for(i = 0; i < NUM_SHARE; i++){
         element_init_G1(ele_att_hash[i], *pairing);
@@ -87,10 +100,20 @@ void initialize(element_t *qR,
 void finalize(element_t *qR,
                 element_t *ele_att_hash,
                 element_t *g,
+                element_t *h,
+                element_t *C,
+                element_t *C_tilde,
                 element_t *Cy,
-                element_t *Cy_prime){
+                element_t *Cy_prime,
+                mpz_t *message_mpz,
+                element_t *message_ele){
     int i = 0;
     element_clear(*g);
+    element_clear(*h);
+    element_clear(*C);
+    element_clear(*C_tilde);
+    mpz_clear(*message_mpz);
+    element_clear(*message_ele);
     for(i = 0; i < NUM_SHARE; i++){
         element_clear(ele_att_hash[i]);
         element_clear(qR[i]);
@@ -105,13 +128,21 @@ int main(void){
     //library
     pairing_t pairing;
     element_t g;
+    element_t h;
     element_t *ele_att_hash;
     element_t *qR;
+    element_t *C;
+    element_t *C_tilde;
     element_t *Cy;
     element_t *Cy_prime;
+    element_t *message_ele;
     char s[16384];
     FILE *fp;
-    int i = 0;  
+    int i = 0; 
+    int num_read = 0;
+    char *str_read, *str1;
+    char *m;
+    mpz_t message_mpz;
     //initialize the pairing
     fp = fopen("../public/a.param", "r");
     if (!fp) 
@@ -127,11 +158,20 @@ int main(void){
     //memory allocation starts here
     qR = (element_t *)malloc(sizeof(element_t)*(NUM_SHARE + 1)); 
     ele_att_hash = (element_t *)malloc(sizeof(element_t)*NUM_SHARE);
+    C = (element_t *)malloc(sizeof(element_t));
+    C_tilde = (element_t *)malloc(sizeof(element_t));
     Cy = (element_t *)malloc(sizeof(element_t)*NUM_SHARE);
     Cy_prime = (element_t *)malloc(sizeof(element_t)*NUM_SHARE);
-    initialize(qR, ele_att_hash, &pairing, &g, Cy, Cy_prime);
+    str_read = (char *)malloc(sizeof(char)*500);
+    str1 = (char *)malloc(sizeof(char)*3);
+    m = (char *)malloc(sizeof(char)*512);
+    message_ele = (element_t *)malloc(sizeof(element_t));
+    //initialize the elements to the coresponding type
+    initialize(qR, ele_att_hash, &pairing, &g, &h, C, C_tilde, Cy, Cy_prime,
+    &message_mpz, message_ele);
 
     //randomly pick the nodes' values and save them into a file
+    
     fp = fopen("nodes_value_of_tree.txt", "w+");
     if(!fp)
         pbc_die("error opening the tree value file");
@@ -145,13 +185,78 @@ int main(void){
     
     element_set(g, ((curve_data_ptr)((a_pairing_data_ptr)
     pairing->data)->Eq->data)->gen);
-    /*here is the encryption part*/
+    //here is the encryption part
+    //compute Cs and C_primes
     for(i = 0; i < NUM_SHARE; i++){
         element_pow_zn(Cy[i], g, qR[i+1]);    
         element_pow_zn(Cy_prime[i], ele_att_hash[i], qR[i+1]);
     }
-    //read out the public key of owner
-    //read out the message and multiply it by e(g,g)^(alpha*s)  
-    finalize(qR, ele_att_hash, &g, Cy, Cy_prime); 
+    //read out the public key h of authorizer
+    fp = fopen("../public/authorizer_public_keys.txt", "r");
+    if(!fp)
+        pbc_die("open authorizer public key file failed");
+    else{
+        while(true){
+            fgets(str_read, 500, fp);
+            strncpy(str1, str_read, 2);
+            if(strstr(str_read, "h:"))
+                break;
+        }
+        str_read += 2;
+        element_set_str(h, str_read, 10);
+        fclose(fp);
+    }
+    //compute C
+    element_pow_zn(*C, h, qR[0]);
+    //read out the owner public key e(g, g)^alpha 
+    fp = fopen("../public/owner_public_key.txt", "r");
+    if(!fp)
+        pbc_die("open owner public key file failed");
+    else{
+        while(true){
+            fgets(str_read, 500, fp);
+            strncpy(str1, str_read, 2);
+            if(strstr(str_read, "public key:"))
+                break;
+        }
+        str_read += 11;
+        element_set_str(*C_tilde, str_read, 10);
+        //printf("%s\n", str_read);
+        fclose(fp);
+    }
+    element_pow_zn(*C_tilde, *C_tilde, qR[0]);//e(g, g)^(alpha*s)
+    
+    //read out the message and  
+    fp = fopen("../public/message.txt", "r");
+    if(!fp)
+        pbc_die("fail to open the message file");
+    else{
+        while(true){
+            num_read = fread(str_read, 1, 100, fp);
+            str_read[num_read] = '\0';
+           // printf("%s%d\n", str_read, num_read);
+           //we need to initialize m_mpz to 0 in order to start a another round
+           //of encryption.
+           mpz_init(message_mpz);
+           messageToValue(str_read, message_mpz, m);
+           
+           strcpy(str_read, "[");
+           strcat(str_read, m);
+           strcat(str_read, ",0]");
+           element_set_str(*message_ele, str_read, 10);
+           //multiply it by e(g,g)^(alpha*s)
+           element_mul(*C_tilde, *C_tilde, *message_ele);
+           //write the encrypted data to file
+           if(feof(fp)){
+                break;
+            }
+     /*       if(ferror(fp))
+                pbc_die("reading error occurs");
+        */
+        }
+        fclose(fp);
+    }
+    finalize(qR, ele_att_hash, &g, &h, C, C_tilde, Cy, Cy_prime, &message_mpz,
+    message_ele); 
     return 0;
 }
